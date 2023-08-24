375e709d2e1f45777a4c150e9b3fa6d23fcc9033
Andy Cai <huayicai@gmail.com>
2023-08-24 18:04:04 +0800

    使用 useReducer 重构代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { resolveResource } from "@tauri-apps/api/path";
 import { readTextFile } from "@tauri-apps/api/fs";
 import "./App.css";
 
 enum Status {
   Idle = 0,
+  Pause,
+  Tick,
+}
+
+enum Action {
+  Pause = 1,
+  Reset,
+  Ready,
+  Tick,
+}
+
+enum WorkType {
   Work = 1,
-  Break = 2
+  Break,
 }
 
 const ONE_MINUTE = 60;
+const INTERVAL = 1000;
 const resourcePath = await resolveResource("resources/data.json");
 const data = JSON.parse(await readTextFile(resourcePath));
 
@@ -22,16 +35,10 @@ if (localStorage.getItem("defaultBreakDuration") === null) {
 }
 
 const defaultWorkDuration = Number(localStorage.getItem("defaultWorkDuration"));
-// const workDurationList = data.workDurationList;
 const defaultBreakDuration = Number(localStorage.getItem("defaultBreakDuration"));
-// const breakDurationList = data.breakDurationList;
-let count = defaultWorkDuration;
-let starting = false;
 let ticker : any;
-let nextActionStatus = Status.Work;
-
-console.info("defaultWorkDuration: ", defaultWorkDuration);
-console.info("defaultBreakDuration: ", defaultBreakDuration);
+let globalCount = defaultWorkDuration;
+let globalWorkType = WorkType.Work;
 
 /**
  * 转换秒数为显示：分:秒
@@ -40,90 +47,113 @@ function convertCount(count: number) : string {
   return (`${Math.floor(count / ONE_MINUTE)}:${Math.floor(count % ONE_MINUTE) < 10 ? "0" : ""}${count % ONE_MINUTE}`);
 }
 
-function App() {
-  const [title, setTitle] = useState("Work");
-  const [countValue, setCountValue] = useState(convertCount(defaultWorkDuration));
-  const [buttonName, setButtonName] = useState("Start");
+const initialState = {
+  title: "Work",
+  showCount: convertCount(defaultWorkDuration),
+  status: Status.Idle,
+  buttonName: "Start",
+}
 
-  // useEffect(() => {
-  //   ticker = setInterval(() => {
-  //     setCount(preCount => preCount - 1);
-  //     console.info("left: ", count);
-  //   }, 1000);
-
-  //   return ()=> clearInterval(ticker);
-  // }, []);
-
-  async function start() {
-    if (starting) {
-      stop()
-      return;
-    }
-    starting = true;
-    setButtonName("Stop");
-
-    ticker = setInterval(() => {
-      count -= 1;
-      setCountValue(convertCount(count));
-      if (count <= 0) {
-        stop();
-        updateStatus();
+function workReducer(state: any, action: any) {
+  switch (action.type) {
+    case Action.Pause:
+      clearInterval(ticker);
+      return {
+        ...state,
+        status: Status.Pause,
+        buttonName: "Start",
+      };
+    case Action.Reset:
+      clearInterval(ticker);
+      globalCount = defaultWorkDuration;
+      return {
+        ...state,
+        status: Status.Idle,
+        title: "Work",
+        type: WorkType.Work,
+        showCount: convertCount(defaultWorkDuration),
+        buttonName: "Start",
+      };
+    case Action.Ready:
+      return {
+        ...state,
+        status: Status.Tick,
+        buttonName: "Pause",
       }
-    }, 1000);
+    case Action.Tick:
+      if (action.count < 0) {
+        clearInterval(ticker);
+        if (action.workType === WorkType.Work) {
+          globalCount = defaultWorkDuration;
+          state.title = "Work";
+          state.showCount = convertCount(globalCount);
+        } else {
+          globalCount = defaultBreakDuration;
+          state.title = "Break";
+          state.showCount = convertCount(globalCount);
+        }
+        return {
+          ...state,
+          status: Status.Idle,
+          buttonName: "Start",
+        }
+      } else {
+        return {
+          ...state,
+          status: Status.Tick,
+          count: action.count,
+          showCount: convertCount(action.count),
+          buttonName: "Pause",
+        }
+      }
+    default:
+      return state;
   }
+}
 
+function App() {
+  const [state, dispatch] = useReducer(workReducer, initialState);
+  const {title, showCount, status, buttonName} = state;
 
-  /**
-   * 改变当前状态
-   */
-  function updateStatus() {
-    if (nextActionStatus === Status.Work) {
-      setCountValue(convertCount(defaultBreakDuration));
-      setTitle("Break");
-      count = defaultBreakDuration;
-      nextActionStatus = Status.Break;
+  async function clickStart() {
+    if (status === Status.Tick) {
+      dispatch({type: Action.Pause});
     } else {
-      setTitle("Work");
-      setCountValue(convertCount(defaultWorkDuration));
-      count = defaultWorkDuration;
-      nextActionStatus = Status.Work;
+      dispatch({type: Action.Ready});
+      clearInterval(ticker);
+      ticker = setInterval(() => {
+        globalCount -= 1;
+        console.info("work type 0: ", globalWorkType, globalWorkType === WorkType.Work);
+        if (globalCount < 0) {
+          console.info("work type 1: ", globalWorkType, globalWorkType === WorkType.Work);
+          globalWorkType = ((globalWorkType === WorkType.Work) ? WorkType.Break : WorkType.Work);
+          console.info("work type 2: ", globalWorkType, globalWorkType === WorkType.Work);
+        }
+        dispatch({type: Action.Tick, count: globalCount, workType: globalWorkType});
+      }, INTERVAL);
     }
   }
 
-  /**
-   * 停止计时
-   */
-  function stop() {
-    setButtonName("Start");
-    starting = false;
-    clearInterval(ticker);
-  }
-
-  /**
-   * 重置计时
-   */
-  async function reset() {
-    setButtonName("Start");
-    setCountValue(convertCount(defaultWorkDuration));
-    count = defaultWorkDuration;
-    starting = false;
-    clearInterval(ticker);
+  async function clickReset() {
+    dispatch({type: Action.Reset});
   }
 
   // async function setDefault() {
-  //   localStorage.setItem("defaultWorkDuration", workDurationList[2]);
-  //   localStorage.setItem("defaultBreakDuration", breakDurationList[0]); 
+  //   localStorage.setItem("defaultWorkDuration", "10");
+  //   localStorage.setItem("defaultBreakDuration", "5"); 
   // }
 
   return (
     <div className="container">
       <div className="content">
-      <h2 className="title">{title}</h2>
-      <h1 className="time">{countValue}</h1>
+      <h4 className="title">{title}</h4>
+      <h1 className="time">{showCount}</h1>
       </div>
-      <div>
-      <button type="button" onClick={start}>{buttonName}</button>
-      <button type="button" onClick={reset}>Reset</button>
+      <div className="start-op">
+      <button type="button" onClick={clickStart}>{buttonName}</button>
+      </div>
+      <div style={{display:status !== Status.Idle?"block":"none"}} className="reset-op">
+      <button type="button" onClick={clickReset}>Reset</button>
       {/* <button type="button" onClick={setDefault}>Default</button> */}
       </div>
     </div>
