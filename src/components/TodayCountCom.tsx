c708e35d6575011b08625824d5f5c986f4d665a3
Andy Cai <huayicai@gmail.com>
2023-08-31 18:51:34 +0800

    代码优化,调整当前完成番茄钟位置

--- src/components/TodayCountCom.tsx
+++ src/components/TodayCountCom.tsx
@@ -9,7 +9,7 @@ function TodayCountCom() {
     return (
       <div className="absolute top-1 left-1 flex align-bottom">
         <ClockOutlineIcon className="" size={22} />
-        <span className="text-xs">x{todayCount}</span>
+        <span className="text-xs pt-1">x{todayCount}</span>
       </div>
     );
 }
