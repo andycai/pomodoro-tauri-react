a3394a58e825572e867df187ffcaf4447bc0c0c4
Andy Cai <huayicai@gmail.com>
2023-09-03 23:12:10 +0800

    界面优化,放大时间显示

--- src/components/TimeCounterCom.tsx
+++ src/components/TimeCounterCom.tsx
@@ -5,7 +5,7 @@ import { convertTimeString } from "../utils";
 function TimeCounterCom() {
   const [count, workType] = useCountStore((state) => [state.count, state.workType]);
   console.log("render TimeCounter", workType);
-  const s = "antialiased pt-1 text-7xl font-black text-center ";
+  const s = "antialiased pt-0 text-8xl font-black text-center ";
 
   return (
     <h1 className={`${workType === WorkType.Work ? s+"text-red-600" : s+"text-green-600"}`}>{convertTimeString(count)}</h1>
